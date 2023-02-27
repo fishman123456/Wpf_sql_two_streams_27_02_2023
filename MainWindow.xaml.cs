d1061ab9ad34e1411a5109e9f6ed4543c3e909d4
denis <106389581+fishman123456@users.noreply.github.com>
2023-02-27 20:55:21 +0700

    переделать
    нихера не получилось

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
-using Microsoft.Data.SqlClient;
-using Microsoft.Data.Sql;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace Wpf_sql_two_streams_27_02_2023
 {
@@ -33,31 +32,62 @@ namespace Wpf_sql_two_streams_27_02_2023
         "Initial Catalog = library; Integrated Security = true");
         public string sqlsel1 = "SELECT * FROM Books";
         public string sqlsel2 = "SELECT * FROM Authors";
-        public void LoadData(string sqlsel,ref DataGrid DG)
+        public async Task LoadDataAsync(string sqlsel)
         {
+            DataTable dt = new DataTable();
+
             SqlConnection con = null;
             MessageBox.Show($"{sqlsel} началось");
             //описываем соединение 
             con = new SqlConnection(connectString);
+            // создаем асинхронное подключение
+           
+            await con.OpenAsync();
+
             //создаем комманду на соединение
             SqlCommand cmd = new SqlCommand(sqlsel, con);
             //открываем соединение
-            con.Open();
-            SqlDataAdapter sda = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            sda.Fill(dt);
-           DG.ItemsSource = dt.DefaultView;
-            //выполнить запрос, занесенный
-            //в объект command
-            MessageBox.Show($"{sqlsel} кончилось");
+           SqlDataReader data = cmd.ExecuteReader();
 
+            using (SqlDataReader reader = await cmd.ExecuteReaderAsync())
+            {
+                int line = 0;
+                do
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        if (line == 0)
+                        {
+                            for (int i = 0; i <
+                            reader.FieldCount; i++)
+                            {
+                                dt.Columns.Add(reader.
+                                GetName(i));
+                            }
+                            line++;
+                        }
+                        DataRow row = dt.NewRow();
+                        for (int i = 0; i < reader.
+                        FieldCount; i++)
+                        {
+                            row[i] = await reader.
+                            GetFieldValueAsync<Object>(i);
+                        }
+                       dt.Rows.Add(row);
+                    }
+                } while (reader.NextResult());
+
+                //выполнить запрос, занесенный
+                //в объект command
+                MessageBox.Show($"{sqlsel} кончилось");
+            }
             con.Close();
         }
 
         private void BT_1_Click(object sender, RoutedEventArgs e)
         {
-            LoadData(sqlsel1, ref DG_1);
-            LoadData(sqlsel2, ref DG_2);
+            LoadDataAsync(sqlsel1);
+            LoadDataAsync(sqlsel2);
         }
     }
 }
